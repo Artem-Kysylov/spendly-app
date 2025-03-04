e6edaab1add568832593b39c074ab7f5e98f2344
MACBOOK AIR <macbookair@MACBOOKs-MacBook-Air.local>
2025-03-04 20:07:47 +0200

    working with auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 // Imports 
+import { useState, useEffect } from 'react'
+import { supabase } from './lib/supabaseClient'
+import { Session, Subscription } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
 import './index.css'
 
 // Import components 
 import Button from "./components/Button"
 
 function App() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    }: { data: { subscription: Subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+  }
+  else {
+    return (<div>Logged in!</div>)
+  }
+ 
   return (
     <>
       <h1>Explore Spendly app</h1>
