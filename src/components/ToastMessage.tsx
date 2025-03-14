4bcb17db084fee3cd8f5317d447881cad7ab5945
MACBOOK AIR <macbookair@MACBOOKs-MacBook-Air.local>
2025-03-14 12:13:43 +0200

    add toast message component

---
+++ src/components/ToastMessage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ToastMessage = () => {
+  return (
+    <div className="toast">
+        <div className="alert alert-success">
+            <span>New message arrived.</span>
+        </div>
+    </div>
+  )
+}
+
+export default ToastMessage
