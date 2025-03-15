02999edb8ed5df26ad164a5cbc6625f2e1cb54a3
MACBOOK AIR <macbookair@MACBOOKs-MacBook-Air.local>
2025-03-15 16:17:43 +0200

    add handledelete function

--- src/components/ToastMessage.tsx
+++ src/components/ToastMessage.tsx
@@ -3,10 +3,8 @@ import { ToastMessageProps } from '../types/types'
 
 const ToastMessage = ({ text, type }: ToastMessageProps) => {
   return (
-    <div className="toast">
-        <div className={`alert alert-${type} text-white rounded-lg`}>
-            <span>{text}</span>
-        </div>
+    <div className={`alert alert-${type} text-white rounded-lg`}>
+        <span>{text}</span>
     </div>
   )
 }
--- src/components/TransactionsTable.tsx
+++ src/components/TransactionsTable.tsx
@@ -1,31 +1,57 @@
 // Imports 
 import { useEffect, useState } from 'react'
 import { supabase } from '../lib/supabaseClient'    
-import { UserAuth } from '../context/AuthContext'
-import { Transaction } from '../types/types'
+import { ToastMessageProps, Transaction } from '../types/types'
 
 // Import components 
 import Button from '../components/Button'
+import ToastMessage from './ToastMessage'
 
 const TransactionsTable = () => {
-  const { session } = UserAuth()
- 
   const [transactions, setTransactions] = useState<Transaction[]>([])
+  const [toastMessage, setToastMessage] = useState<ToastMessageProps | null>(null)
+
+  const fetchTransactions = async () => {
+    const { data, error } = await supabase.from('Transactions').select('*')
+    setTransactions(data as Transaction[])
+    if (error) {
+      console.error('Error fetching transactions:', error)
+    }
+  }
 
   useEffect(() => {
-    const fetchTransactions = async () => {
-      const { data, error } = await supabase.from('Transactions').select('*')
-      setTransactions(data as Transaction[])
-      if (error) {
-        console.error('Error fetching transactions:', error)
-      }
-    }
     fetchTransactions()
   }, [])
 
+  const handleDelete = async (id: string) => {
+    try {
+      const { error } = await supabase.from('Transactions').delete().eq('id', id)
+      
+      if (error) {
+        console.error('Error deleting transaction:', error)
+        handleToastMessage('Error deleting transaction', 'error')
+        return
+      }
+      
+      handleToastMessage('Transaction deleted successfully', 'success')
+      await fetchTransactions()
+    } catch (error) {
+      console.error('Unexpected error during deletion:', error)
+      handleToastMessage('An unexpected error occurred', 'error')
+    }
+  }
+
+  const handleToastMessage = (text: string, type: ToastMessageProps['type']) => {
+    setToastMessage({ text, type })
+    setTimeout(() => {
+      setToastMessage(null)
+    }, 3000)
+  }
+  
   return (
     <div>
         <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+            {toastMessage && <ToastMessage text={toastMessage.text} type={toastMessage.type} />}
             <table className="table">
                 {/* head */}
                 <thead>
@@ -50,6 +76,7 @@ const TransactionsTable = () => {
                                 <Button
                                     text="Delete"
                                     className='btn-ghost text-error'
+                                    onClick={() => handleDelete(transaction.id)}
                                 />
                             </td>
                         </tr>
