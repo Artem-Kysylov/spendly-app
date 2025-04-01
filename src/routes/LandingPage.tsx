9487202e3753d72f799469ba8ee8299f94710325
MACBOOK AIR <macbookair@MACBOOKs-MacBook-Air.local>
2025-04-01 19:56:48 +0300

    add landing page sections

--- src/routes/LandingPage.tsx
+++ src/routes/LandingPage.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
-
 // Import sections 
+import Header from '../components/landing-sections/Header'
 import Hero from '../components/landing-sections/Hero'
-
+import HowItWorks from '../components/landing-sections/HowItWorks'
+import WhyChooseUs from '../components/landing-sections/WhyChooseUs'
+import Cta from '../components/landing-sections/Cta'  
+import Footer from '../components/landing-sections/Footer'
 
 
 const LandingPage = () => {
   return (
     <>
+      <Header/>
       <Hero/>
+      <HowItWorks/>
+      <WhyChooseUs/>
+      <Cta/>
+      <Footer/>
     </>
   )
 }
