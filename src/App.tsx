0e9caeaae1c2f138e9b022772385184f3ddbc08b
MACBOOK AIR <macbookair@MACBOOKs-MacBook-Air.local>
2025-04-10 20:09:07 +0300

    add new routes, start updating app router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,55 @@
 // Imports 
 import './index.css'
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate, Outlet, createBrowserRouter } from "react-router-dom"
 import { UserAuth } from './context/AuthContext'
 
 // Import routes 
 import Dashboard from './routes/Dashboard.tsx'
+import Transactions from './routes/Transactions.tsx'
+import Budgets from './routes/Budgets.tsx'
 import NotFound from './routes/NotFound.tsx'
 import FormPage from './routes/FormPage.tsx'
 import LandingPage from './routes/LandingPage.tsx'
 
 // Import components 
 import ProtectedRoute from './components/ProtectedRoute.tsx'
+import TopBar from './components/TopBar.tsx'
+
 
 function App() {
   const { session } = UserAuth()
+
+  const AppLayout = () => {
+    return (
+      <div>
+        <TopBar/>
+        <div>
+          <Outlet/>
+        </div>
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path:'/dashboard',
+      element: <AppLayout/>,
+      children: [
+        {
+          path: '/dashboard',
+          element: <Dashboard/>
+        },       
+        {
+          path: '/transactions',
+          element: <Transactions/>
+        },       
+        {
+          path: '/budgets',
+          element: <Budgets/>
+        },       
+      ]
+    }
+  ])
 
 return (
     <BrowserRouter>
